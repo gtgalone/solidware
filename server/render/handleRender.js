6549c16c7ec1131444259a213e90e2772601ce0d
juhun seem <jehun.seem@3earthians.com>
2017-12-07 02:28:13 +0900

    jehun-20171206

--- server/render/handleRender.js
+++ server/render/handleRender.js
@@ -3,7 +3,7 @@ const path = require('path');
 const serialize = require('serialize-javascript');
 const render = require('../../dist/server.js').default; // ES6 형식으로 만들어진 모듈이므로, 뒤에 .default 를 붙여주어야합니다.
 // html 내용을 해당 상수에 저장합니다
-const template = fs.readFileSync(path.join(__dirname, '../../src/index.html'), { encoding: 'utf8' });
+const template = fs.readFileSync(path.join(__dirname, '../../dist/index.html'), { encoding: 'utf8' });
 
 const handleRender = (req, res) => {
   // 요청이 들어올 때 현재 경로를 render 함수에 전달시켜서 문자열을 생성합니다
